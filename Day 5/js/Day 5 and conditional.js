e959b1942e968944563546d022a7ac6347a1fde2
FSChrisWatson <watson2chris@yahoo.com>
2015-01-15 18:55:35 -0500

    If then statement
    Will it run or not

--- Day 5/js/Day 5 and conditional.js
+++ Day 5/js/Day 5 and conditional.js
@@ -4,10 +4,31 @@
 /*
  Chris Watson
  1/10/2015
- Day 3 Coding
+ Day 5 Coding
  */
 
 // Single lined comment
 
 //This is an alert
 //alert("Testing to see if this is connected");
+
+// Basic Conditional Statement
+
+//Create a boolean variable to test
+var oldEnough = true;
+
+//If the child is old enough then print to the console, You Can Ride!
+
+/*
+Basic structure of if statement
+
+if(condition to test) {Code to run if the condition is true}
+ */
+
+if(oldEnough){}
+//Code inside of {} will run if the test is true
+//This code will be skipped if test is false
+console.log("You can ride the coaster!");
+}
+
+console.log("What came after the if then statement");
