b34bbf5156ec386ec293b2f6ee2e44c4e220258e
FSChrisWatson <watson2chris@yahoo.com>
2015-01-15 20:52:46 -0500

    Logical Operations
    Learned Logical Operations

--- Day 5/js/Day5Logical Operators.js
+++ Day 5/js/Day5Logical Operators.js
@@ -1,3 +1,44 @@
 /**
  * Created by christopherwatson on 1/15/15.
  */
+// Single lined comment
+
+//This is an alert
+//alert("Testing to see if this is connected");
+
+///true&&true= true
+// true&&false= false
+// false&&true= false
+// false&&false= false
+
+//true||true=true
+// true||false=false
+// false||true=false
+// false||false=false
+
+// if the price of 3DS is less than budget lets buy it
+
+var budget = 100;
+var dsPrice = 169;
+var paycheck = 600;
+
+if(dsPrice<budget && paycheck> 500){
+    console.log("Buy the 3DS!");
+} else{
+    console.log("Do not buy the 3DS! Because you are broke");
+
+}
+
+// Lets buy a smart tv if it is less than our budget OR we won the lottery
+
+var tvPrice = 540
+var tvBudget = 500
+var wonLottery = true;
+
+if(tvPrice>tvBudget || wonLottery){
+
+    console.log("Lets go buy a tv");
+
+} else {
+    console.log("Stick with your small tv!");
+}
