d6b4afae19922c48316198ebdbc00de6c1835938
FSChrisWatson <watson2chris@yahoo.com>
2015-01-15 19:18:32 -0500

    Basic if and Basic Else
    If then statements

--- Day 5/js/Day 5 and conditional.js
+++ Day 5/js/Day 5 and conditional.js
@@ -32,3 +32,20 @@ console.log("You can ride the coaster!");
 }
 
 console.log("What came after the if then statement");
+
+//Relationship Expressions
+
+//If the kid is over 48" then he can run
+var(kidHeight)= 30;
+
+//Create variable for kid height
+var(minHeight)= 48;
+
+if(kidHeight > 48){
+console.log("You are tall enough to ride!");
+} else{
+    //This conde will run if the test is false!
+    console.log("Sorry you are to short!");
+}
+
+
