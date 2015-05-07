ee4d673cc962ad4a92fe618f605177e0545e6575
FSChrisWatson <watson2chris@yahoo.com>
2015-05-07 12:16:25 -0400

    Day two commits 05/2015

---
+++ Day2/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Chris Watson
+ * SDI 1505
+ * Day Two
+ * Fun with Data Types.
+ */
+
+//This is a single-line comment
+
+//These are my variables
+var myHomeAddress = "123 Elm Street";
+var milesToStore = 8.5;
+var construction = true;
+var myFavoriteStore = "Publix Supermarket";
+
+construction = confirm("Is there construction? CLick OK for yes, Cancel for no.")//adding my confirm
+
+//my outputs
+console.log("I live at " + myHomeAddress);
+console.log("I Shop at Piublix " + myFavoriteStore);
+console.log("It's only "+ milesToStore + " to the store. ");
+console.log("It's " + construction + " that there is construction enroute.");
+
