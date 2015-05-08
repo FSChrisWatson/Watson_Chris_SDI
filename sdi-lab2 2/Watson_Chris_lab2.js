62c13db13b99fcd94ad91303157b352a6ad4f56b
FSChrisWatson <watson2chris@yahoo.com>
2015-05-07 22:19:18 -0400

    Day 2 LabWork

---
+++ sdi-lab2 2/Watson_Chris_lab2.js
@@ -0,0 +1,30 @@
+alert("JavaScript works!");
+/**
+ * Chris Watson
+ * SDI 1505
+ * Day Two
+ */
+
+
+//These are my variables
+var myHomeAddress = "103 Wymore Road";
+var milesToFullSail = 5.3;
+var carWreck = false;
+var myFavoriteShoes = "Black Jordan 11's";
+var morningRegime = "face";
+var hygiene = "teeth";
+
+//adding my confirm
+carWreck = confirm("Is the road blocked? CLick OK for yes, Cancel for no.")
+//addding my prompt
+prompt("Do you want to take another route?");
+
+//my outputs
+console.log("I woke up washed my " + morningRegime + " and brushed my " + hygiene);
+console.log("I put on the " + myFavoriteShoes + " because they match my black jeans");
+console.log("I got in my car and drove to school");
+console.log("I live at " + myHomeAddress);
+console.log("It's only "+ milesToFullSail + " miles to Fullsail University. ");
+console.log("It's " + carWreck + " that the road is blocked from a wreck.");
+console.log("So I went another route");
+
