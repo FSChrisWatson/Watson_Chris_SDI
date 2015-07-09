3a24241963589cacabb8808bc25d7948f9417237
FSChrisWatson <watson2chris@yahoo.com>
2015-07-09 19:18:15 -0400

    Creating starting files for day 2
    also created files for day 3

---
+++ Day 2/js/script.js
@@ -0,0 +1,20 @@
+/*
+This is going to be a multi line comment
+Christopher Watson
+7/9/15
+SDI Section 00
+Day 2 coding Basics
+ */
+
+//alert("This is making sure our js file is connected!");
+
+//This is a single lined comment
+//Usually it will describe what comes right after it
+
+/*
+Multi lined comment
+so anything in between the stars and slashes is ignored by the computer
+ */
+
+//Alert - gives the user information but returns NOTHING
+alert("Welcome to Compete Elite");
---
+++ Day 3/js/script.js
@@ -0,0 +1,3 @@
+/**
+ * Created by christopherwatson on 7/9/15.
+ */
