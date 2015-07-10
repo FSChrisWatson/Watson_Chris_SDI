40f409ef7d1488ebfa3aa1be0767f840de85eaab
FSChrisWatson <watson2chris@yahoo.com>
2015-07-09 22:24:46 -0400

    Start of Homework

---
+++ Day 2/Day3LabExpressions/js/script.js
@@ -0,0 +1,26 @@
+/*
+ Christopher Watson
+ 7/9/15
+ SDI Section 00
+ HOMEWORK
+ */
+
+//alert("Just wanted to making sure my js file is connected!");
+
+//console.log - allows programmers to see information
+//console.log ("This is the console.");
+
+//calculate how much pizza each party goer will get at the party.
+
+//Declare a variable
+//Use the keyword of var
+
+
+var slicesPerPizza = 9;
+var numberOfPeople = 15;
+var pizzaOrdered = 3;
+
+//slices of pizza * pizza ordered / number of people
+
+var slicesPerPerson = slicesPerPizza * pizzaOrdered / numberOfPeople;
+console.log("Each person ate " +  slicesPerPerson + " pizzas at the party.");
