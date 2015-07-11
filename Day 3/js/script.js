964557f404c8d4b55c916dd873005d17620c8eb9
FSChrisWatson <watson2chris@yahoo.com>
2015-07-11 18:03:07 -0400

    Basic Array
    Array items and .length

--- Day 3/js/script.js
+++ Day 3/js/script.js
@@ -6,3 +6,20 @@ Arrays
  */
 
 //alert("Hope this shit works?");
+
+//Create a basic array
+//Picking Oranges
+
+var orangeBins = [30, 500, 10002];
+
+//How many oranges did we pick
+var totalOranges = orangeBins [0] + orangeBins [1] + orangeBins [2];
+console.log(totalOranges + " Oranges were picked in three days.");
+
+//Average number of oranges picked
+var averageOranges = totalOranges / 3;
+ //Instead of 3 put = totalOranges / orangeBins.length;
+
+//length property of arrays
+console.log(orangeBins.length);
+console.log("The average number of oranges picked is " + averageOranges);
