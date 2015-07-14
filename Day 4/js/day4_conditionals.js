4e3781fb983c7b5fe246a3cf8f867f3a4110d825
FSChrisWatson <watson2chris@yahoo.com>
2015-07-14 19:52:18 -0400

    if else statements

--- Day 4/js/day4_conditionals.js
+++ Day 4/js/day4_conditionals.js
@@ -26,14 +26,19 @@ if(oldEnough){
  //Relational Expressions - Operators
  //Test the kids height to see if he can ride the ride
 
- var kidHeight = 50;
+ var kidHeight = prompt("How tall are you in inches?");
+ var sneakerLift = 2;
 
  //if the child is 48 or taller they can ride the ride
  if(kidHeight >=48){
      //code inside of here will run if child is 48 or taller
      console.log("Congrats you are tall enough to ride the coaster");
 
+ } else if (kidHeight + sneakerLift >=48){
+     //This code will run if the child is tall enough while wearing sneakers
+     console.log("You are tall enough to ride the coaster, if you wear sneakers!");
+
  } else {
-     //
+     console.log("Sorry you are too short to ride the roller coaster!");
 
  }
