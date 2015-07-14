d2b3530b473509c36203b27624d7c70a57b92174
FSChrisWatson <watson2chris@yahoo.com>
2015-07-14 18:59:16 -0400

    cnoditionals

---
+++ Day 4/js/day4_conditionals.js
@@ -0,0 +1,25 @@
+
+ /*
+Christopher Watson 
+Section 00
+7/14/15
+Conditionals
+ */
+
+//alert("Testing to see if this works!");
+
+ //Basic Conditional if statement
+ //if(condition to test){ code to run if true. }
+
+ //create a boolean
+ var oldEnough = false;
+
+ //if the child is old enough tell him he can ride the coaster
+if(oldEnough){
+    //code inside of the {} will run if oldEnough is a boolean of true
+    console.log("You can ride the coaster!!");
+} else {
+    //THis code will run if the if oldEnough evaluates to a boolean of false
+    console.log("Sorry you are to young to ride");
+}
+ 
