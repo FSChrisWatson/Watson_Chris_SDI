f26b61ee64f0a20dd5fe3cc796a3f2df105bfa2d
FSChrisWatson <watson2chris@yahoo.com>
2015-07-15 02:30:11 -0400

    Conditionals homework

--- Day 4/Conditionals_worksheet/js/script.js
+++ Day 4/Conditionals_worksheet/js/script.js
@@ -5,13 +5,13 @@
  Conditional assignments
  */
 
-alert("TESTING TESTING TESTING to see if this works!");
+//alert("TESTING TESTING TESTING to see if this works!");
 
 //If your cars has more than 200 gallons of gas left than you can make it without stopping!
 
 var mpg = 50;
 var carsGasTank = 250;
-var gasLeft = 100;
+var gasLeft = 30;
 
 if(carsGasTank>200){
 
@@ -24,49 +24,77 @@ if(carsGasTank>200){
 }
 
 
-//If your GPA is greater than 2.0 than you can graduate!
-
-//(Condition to test) ? True Code: false code;
-
-//Normal if else statement
-
-var GPA= 3.8;
-
-if(GPA>2.0){
-
-    console.log("Congrats you can graduate");
-
-} else{
-    console.log("Sorry your GPA is to low");
-}
-
-//ternary way
-(GPA>2.0) ?  console.log("Congrats you can graduate"):console.log("Sorry your GPA is to low");
-
-
 //_______________________________
 
 
-var timeOFMovie = 10;
-var ageOfCustomer = 55;
+//If the username & password are not correct, then and error message will appear
+
+var correctUsername = "Chris";
+var correctPassword = "FullSail";
+var username = prompt ("Please Enter correct Username");
+var password = prompt ("Please enter correct password");
+
+if(username == correctUsername && password == correctPassword)
+{
+    console.log("Welcome " + username);}
+
+else if (username != correctUsername){
+    console.log("User not found. Try again")
+
+} else { console.log("Password does not match our records")
+
+
+}
+
+//________________________________
+
+
+var timeOFMovie = 9;
+var ageOfCustomer = 50;
 var ticketPrice = 12;
-var discountPrice = 5;
-var timeFrame2Hours = 5;
+var discountPrice = 7;
+
 if(ageOfCustomer <= 55 && timeOFMovie >= 10) {
     console.log("The ticket price is $" + discountPrice + ".")
 
 } else {
     console.log("Pay $" + ticketPrice + ".");
-}
+} 
 
-//---------------------------------
-//if the price of the scooter is less than our buget than we can buy it.
-//And if our paycheck is over 400;
 
-if(scooterCost < budget && payCheck>400){
-    console.log("Both the scooter is in budget and our paycheck is high enough");
 
-} else {
-    console.log("Sorry you can buy the scooter!");
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
