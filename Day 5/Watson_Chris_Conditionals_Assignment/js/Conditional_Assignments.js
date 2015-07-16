797952a1760ab5e7434222e020275b457d883527
FSChrisWatson <watson2chris@yahoo.com>
2015-07-16 02:48:52 -0400

    Beginning of Day 5 Conditional Assignment

---
+++ Day 5/Watson_Chris_Conditionals_Assignment/js/Conditional_Assignments.js
@@ -0,0 +1,41 @@
+
+ /*
+Christopher Watson 
+Section 00
+7-16-15
+Day 5 Conditionals Assignment
+ */
+
+alert("Testing to see if this works!");
+
+ //If I have the wedding
+ //Then  “ Financially I will be broke && It’s until death do us part”
+ //Console.log(“”);
+ //Else if “If I say No she might kill me”
+
+ var ringCost = 5500;
+
+ var ceremonyCost = 12000;
+
+ var honeyMoonCost = 3500;
+
+ var monthlySalary = 1500;
+
+ //Find out how long it will take to save up for Marriage
+
+ var totalWeddingCost = ringCost + ceremonyCost + honeyMoonCost;
+console.log(totalWeddingCost);
+
+ var monthsNeededToSave = totalWeddingCost / monthlySalary;
+ console.log("It will take " + monthsNeededToSave + " months to save for my Marriage.");
+
+
+
+ //If I have the wedding
+ //Then  “ Financially I will be broke && It’s until death do us part”
+ //Console.log(“”);
+ //Else if “If I say No she might kill me”
+ //Console.log(“”);
+ //Else “Don’t show up and be single for the rest of my life”
+
+
