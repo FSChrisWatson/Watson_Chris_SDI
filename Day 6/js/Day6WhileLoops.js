87b36cbea04a0107016e29c45d39e2852aec9abe
FSChrisWatson <watson2chris@yahoo.com>
2015-07-18 20:26:21 -0400

    While loops

---
+++ Day 6/js/Day6WhileLoops.js
@@ -0,0 +1,20 @@
+
+ /*
+Christopher Watson 
+Section 00
+7/18/15
+While Loops */
+
+//alert("Testing to see if this works!");
+
+ //Initialize a counter variable
+ var counter = 0;
+
+ //while loop with condition
+ //run as long as the condition is true
+ while(counter<200){
+     console.log("The current value is " + counter);
+     //Change the value of the counter variable
+     counter+=5;
+     //Must have the value or you will loop forever
+ }
