13faf0e1153be171413262034f01c58d3cec8ff7
FSChrisWatson <watson2chris@yahoo.com>
2015-07-21 18:19:23 -0400

    Complete code of bottles of beers

--- Day 7/js/day7_beer.js
+++ Day 7/js/day7_beer.js
@@ -10,6 +10,19 @@ Day 7 Beer
 
  //Create a loop that will write out the lyrics for the 99 bottles of beer song
 
+ var numBeers = prompt("How many beers are on the wall?");
+ //validate user prompt
+ while(isNaN(numBeers) || numBeers===""){
+ if(isNaN(numBeers)){
+     numBeers=prompt("Please only type in numbers.\n How many beers are on your wall?");
+ }else{
+     numBeers=prompt("Please do not leave blank.\n How many beers are on your wall?")
+ }
+ }
+
+ //cast the variable as a number
+ numBeers=Number(numBeers);
+
  //for(initialization; condition; increment of change){}
  for(var i=99; i>0; i--){
      //verify that the loop works
@@ -17,10 +30,10 @@ Day 7 Beer
      if(i===2) {
          console.log(i + " bottles of beer on the wall. " + i + " bottles of beer. " +
          " You take one down and pass it around." + (i - 1) + " bottles of beer on the wall");
-     }else if(i===1)
+     }else if(i===1){
          console.log(i + " bottles of beer on the wall. " + i + " bottles of beer. " +
-         " You take one down and pass it around.No more bottles of beer on the wall./n The End");){
-
+         " You take one down and pass it around.No more bottles of beer on the wall./n The End");{
+         }
      }else {
          console.log(i + " bottles of beer on the wall. " + i + " bottles of beer. " +
      " You take one down and pass it around." + (i - 1) + " bottles of beer on the wall");
