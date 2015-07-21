1e6e17677b61e4a029a4e0c844d979e336ac0eb4
FSChrisWatson <watson2chris@yahoo.com>
2015-07-21 18:49:03 -0400

    For Loop version for 8 days

--- Day 7/js/day7_Zombies.js
+++ Day 7/js/day7_Zombies.js
@@ -2,8 +2,33 @@
  /*
 Christopher Watson 
 Section 00
-Date
-Class
+7/21/15
+DAY 7 ZOMBIES
  */
 
-alert("Testing to see if this works!");
+//alert("Testing to see if this works!");
+
+ //We have 1 zombie at Full Sail
+ //It can bite 4 ppl a day and turn them into zombies buy the next day
+ //The CBC wants to know how many zombies there will be in 8 days
+
+ //Create the givens
+ //How many Zombies do we have
+ var numZombies = 1;
+
+ //How many bites can a zombie make each day
+ var numBites = 4;
+
+ //What is our timeline
+ var days = 8;
+
+ /*for(var i=1; i<=days; i++){
+     //How many ppl get infected everyday
+     var infectedPeople = numZombies * numBites;
+
+     //How many zombies will we have at the end of the day?
+     numZombies+=infectedPeople;
+
+     console.log("On day number " + i+ " there will be " + numZombies + " zombies!")
+ }
+     */
