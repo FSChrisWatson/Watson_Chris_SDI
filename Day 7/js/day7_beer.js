e964c9ac9ec6547488608923651af5c0fc37222c
FSChrisWatson <watson2chris@yahoo.com>
2015-07-21 18:08:53 -0400

    Day 7 Bottles of beer

--- Day 7/js/day7_beer.js
+++ Day 7/js/day7_beer.js
@@ -6,4 +6,24 @@ Section 00
 Day 7 Beer
  */
 
-alert("Testing to see if this works!");
+//alert("Testing to see if this works!");
+
+ //Create a loop that will write out the lyrics for the 99 bottles of beer song
+
+ //for(initialization; condition; increment of change){}
+ for(var i=99; i>0; i--){
+     //verify that the loop works
+     //console.log(i);
+     if(i===2) {
+         console.log(i + " bottles of beer on the wall. " + i + " bottles of beer. " +
+         " You take one down and pass it around." + (i - 1) + " bottles of beer on the wall");
+     }else if(i===1)
+         console.log(i + " bottles of beer on the wall. " + i + " bottles of beer. " +
+         " You take one down and pass it around.No more bottles of beer on the wall./n The End");){
+
+     }else {
+         console.log(i + " bottles of beer on the wall. " + i + " bottles of beer. " +
+     " You take one down and pass it around." + (i - 1) + " bottles of beer on the wall");
+
+     }
+ }
