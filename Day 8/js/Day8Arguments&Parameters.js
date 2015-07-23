4c1c41d34ba05e7270cb079420292e9834cd9187
FSChrisWatson <watson2chris@yahoo.com>
2015-07-23 19:25:18 -0400

    Learned how to put info into our functions
    Arguments and Parameters

--- Day 8/js/Day8Arguments&Parameters.js
+++ Day 8/js/Day8Arguments&Parameters.js
@@ -15,22 +15,62 @@ Class
  var width = 10;
  var height = 20;
 
- function calcArea(){
+ //W and H below signifies parameters
+ function calcArea(w,h){
 
      //Create variables
      //width = 10;
      //height = 20;
 
      //calculate the area
-     var area = width*height;
+     var area = w*h;
 
      //console log the area
-     console.log("The area of a rectangle with a width of "+width+" and a height of "+height+" is "+area+".");
+     console.log("The area of a rectangle with a width of "+w+" and a height of "+h+" is "+area+".");
  }
  //Now call the function for it to run in the console
- calcArea();
- //Change the variables
- width=50;
- height=10;
+ //Width and height signifies arguments and arguments go in the function call
+ calcArea(width,height);
 
- calcArea();
+ //Find the area with a width = 6 height = 7
+ calcArea(6,7);
+
+ //Now ask the user
+ var userWidth=prompt("Lets calculate the area of a rectangle.\nPlease enter a width.");
+
+ //validate & number cast the input
+ var userHeight=prompt("Please enter a height.");
+
+ //How would I get a area
+ calcArea(userWidth,userHeight);
+
+
+ //Create a function that will calculate dog years
+
+ function dogYears(humanAge){
+     //Input a human year and spit out a dog year
+     //humanAge*7 = dog age
+     var dogAge=humanAge*7;
+
+     //Print out results
+     console.log("A dog who is "+humanAge+" is "+dogAge+" in dog years.");
+
+ }
+ //Calculate how old a 4 year old dog is in dog years
+dogYears(4);
+
+ //calculate a 7 year dog
+ dogYears(7);
+
+ //Prompt the user for there dogs age
+ var yourDogAge=prompt("Lets figure out how old your dog is in dog years.\nPlease enter your dogs human age.");
+ //validate and cast if needed
+ 
+ dogYears(yourDogAge);
+
+
+
+
+
+
+
