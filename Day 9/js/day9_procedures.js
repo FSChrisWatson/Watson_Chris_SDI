6bdc14781adf76c5d8c6043a72d3fd87ca9b568e
FSChrisWatson <watson2chris@yahoo.com>
2015-07-25 19:00:39 -0400

    Procedures
    The don’t return anything

--- Day 9/js/day9_procedures.js
+++ Day 9/js/day9_procedures.js
@@ -6,4 +6,28 @@ Section 00
 Procedures
  */
 
-alert("Testing to see if this works!");
+//alert("Testing to see if this works!");
+
+ //This is a normal function
+
+ function calcArea(w, h){
+     var area = w*h;
+     return area;
+     //Returns a variable
+ }
+
+ ///function call to calcArea
+ calcArea(10,20);
+ //catch the return value
+ console.log(results1);
+
+ //This is a Procedure
+ //A list of steps to do
+ //RETURNS NOTHING
+ function calcPeri(w,h){
+     var peri = 2*w + 2*h;
+     console.log(peri);
+ }
+ //Function call for the procedure in order for it to run
+ //Does not catch anything!
+calcPeri(10,20);
