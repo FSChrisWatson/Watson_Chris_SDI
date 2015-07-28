ec14b6b507aefab90bb95fe31ebedec4418db3c9
FSChrisWatson <watson2chris@yahoo.com>
2015-07-28 03:23:14 -0400

    finished the anon function

--- Day 9/js/Watson_Chris_Functions_Assignment.js
+++ Day 9/js/Watson_Chris_Functions_Assignment.js
@@ -8,9 +8,6 @@ Class
 
 //alert("Testing to see if this works!");
 
-
-
-
  //Create a function to calculate the number of total shots total Kobe Bryant takes.
  function kobeShots (f,m,t){
    var totalShots = f+m+t;
@@ -18,14 +15,24 @@ Class
  };
 
  //Ask the user will they like to calculate the number of shots Kobe took
- var userPrompt=prompt("Would you like to calculate the shots Kobe took?\n Please answer yes or no");
+ var freeThrows=parseInt(prompt("How many free throws did Kobe take this season?\n Please enter a number"));
  //validate
- while(userPrompt.toLowerCase()!="yes" && userPrompt.toLowerCase()!="no"){
-     userPrompt=prompt("Please only answer yes or no.\nWould you like to calculate the number of shots Kobe took?");
- }
+ while(isNaN(freeThrows) || freeThrows===""){
+     freeThrows=prompt("Please only type numbers.\nHow many free throws did Kobe take this season?")
+ };
 
- //Function call for Anonymous Functions MUST BE AFTER THE DEFINITION!!
- //Function call to total shots
- var resultsTotal = kobeShots(freeThrows,midRange,threes);
- console.log("Kobe took "+f+"free throws,"+m+"mid range shots, and"+t+"threes. He took a total of "+resultsTotal+"shots all together");
-alert("Kobe took "+f+"free throws,"+m+"mid range shots, and"+t+"threes.\n He took a total of "+resultsTotal+"shots all together");
+ var midRange=parseInt(prompt("How many mid-range shots did Kobe take this season?\n Please enter a number"));
+ while(isNaN(midRange) || midRange===""){
+     midRange=prompt("Please only type numbers.\nHow many mid-range shots did Kobe take this season?")
+ };
+
+ var threePointers=parseInt(prompt("How many three pointers did Kobe take this season?\n Please enter a number"));
+ while(isNaN(threePointers) || threePointers===""){
+     threePointers=prompt("Please only type numbers.\nHow many three pointers did Kobe take this season?")
+ };
+
+//Function call for Anonymous Functions MUST BE AFTER THE DEFINITION!!
+//Function call to total shots
+ var resultsTotal = kobeShots(freeThrows,midRange,threePointers);
+ console.log("Kobe took a total of "+resultsTotal+" shots all together!");
+ alert("Kobe took a total of "+resultsTotal+" shots all together!");
